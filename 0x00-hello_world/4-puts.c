f3de472a2b5cf631d12c8088adde18bb1b0f5b40
aymanebenhima <aymanebenhima@gmail.com>
2023-08-10 17:03:37 -0700

    print simple sentence hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,15 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with put function
+ *
+ * You are not allowed to use printf
+ *
+ * Return: Always 0 (Success)
+*/
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
