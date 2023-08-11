6213e7046a1a054e0360d13484685603a295426d
aymanebenhima <aymanebenhima@gmail.com>
2023-08-11 06:37:16 -0700

    positive or negative algorithm

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print value of n satus; zero, positive or negative
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n < 0)
+		printf("%i is negative\n", n);
+	else if (n > 0)
+		printf("%i is positive\n", n);
+	else
+		printf("%i is zero\n", n);
+
+	return (0);
+}
