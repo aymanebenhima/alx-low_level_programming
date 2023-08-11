6fc50cf0a00f5b6a4ad0815f643ac852d8a87760
aymanebenhima <aymanebenhima@gmail.com>
2023-08-10 17:09:03 -0700

    hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with printf function
+ *
+ * You are not allowed to use the function puts
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
