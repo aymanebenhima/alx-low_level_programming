b2996fa00582396021a61f5069777578a27f88ba
aymanebenhima <aymanebenhima@gmail.com>
2023-08-24 19:25:16 -0700

    rot13

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -6,3 +6,7 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *s);
 char *cap_string(char *s);
 char *leet(char *s);
+char *rot13(char *);
+void print_number(int);
+char *infinite_add(char *, char *,char *, int);
+void print_buffer(char *, int);
